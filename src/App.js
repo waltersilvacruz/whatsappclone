7147d962e66cc76413dd8ec6ad97ff25684f4a95
Walter Cruz <walter@tnx.com.br>
2020-10-12 12:24:27 -0400

    Styling application's sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+import ChatItemList from './components/ChatListItem';
+
+//icones
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+  const [chatlist, setChatList] = useState([
+    {}, {}, {}, {}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img src="https://img.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg?size=338&ext=jpg&ga=GA1.2.708001390.1599523200" alt="" className="header--avatar" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: '#919191'}} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
+        </div>
+
+        <div className="chatlist">
+        {chatlist.map((item, key)=> (
+          <ChatItemList 
+            key={key}
+          />
+        ))}
+        </div>
+      </div>
+      <div className="contentarea">        
+        ... content
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ChatItemList.css';
+
+export default () => {
+    return (
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                    <div className="chatListItem--name">Walter Cruz</div>
+                    <div className="chatListItem--date">19:25</div>
+                </div>
+                <div className="chatListItem--line">
+                    <div className="chatListItem--lastMsg">
+                        <p>Tranquilo, e você? Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam libero dignissimos iure repudiandae optio reprehenderit rem! Nemo consequuntur amet nisi itaque magni fuga libero! Suscipit, quos? Vitae, cum consectetur!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
