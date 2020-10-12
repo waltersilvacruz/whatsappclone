0ce73e5a8f97df1f34a73b72a89f243dfceaae86
Walter Cruz <walter@tnx.com.br>
2020-10-12 13:51:26 -0400

    Styling emoji picker

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+
 import './ChatWindow.css';
 
 // icones
@@ -11,6 +13,21 @@ import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 
 export default ()=> {
+
+    const [ emojiOpen, setEmojiOpen ] = useState(false);
+
+    const handleEmojiClick = () => {
+
+    };
+
+    const handleOpenEmoji = () => {
+        setEmojiOpen(true);
+    };
+
+    const handleCloseEmoji = () => {
+        setEmojiOpen(false);
+    };
+
     return (
         <div className="chatWindow">
             <div className="chatWindow--header">
@@ -38,10 +55,31 @@ export default ()=> {
 
             </div>
 
+            <div 
+                className="chatWindow--emojiarea"
+                style={{height: emojiOpen ? '200px' : '0px'}}
+                >
+                <EmojiPicker
+                    onEmojiClick={handleEmojiClick}
+                    disableSearchBar
+                    disableSkinTonePicker
+                />
+            </div>
+
             <div className="chatWindow--footer">
                 <div className="chatWindow--pre">
-                    <div className="chatWindow--btn">
-                        <InsertEmoticonIcon style={{color: '#919191'}} />
+                    <div className="chatWindow--btn" style={{width: emojiOpen ? '40px' : '0px'}}>
+                        <CloseIcon 
+                        style={{color: '#919191'}} 
+                        onClick={handleCloseEmoji}
+                        />
+                    </div>
+
+                    <div 
+                    className="chatWindow--btn"
+                    onClick={handleOpenEmoji}
+                    >
+                        <InsertEmoticonIcon style={{color: emojiOpen ? '#009688' : '#919191'}} />
                     </div>
                 </div>
 
